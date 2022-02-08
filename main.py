261c613b2ce70325c998f0449c8d4db9ea367896
Mateos <xavimateosm@gmail.com>
2022-02-08 16:57:29 +0100

    Carga de datos

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-print("cosas2  patos")
-f = open("input/SOKOBOTTO1.txt",'r')
-l = f.read().replace('\n','').split(",")
-print(l)
+def cargaDatos(fichero):
+    #Abrimos el fichero
+    f = open(fichero,'r')
+    #Leemos el fichero entero, quitamos todos los saltos de línea, separamos por comas y quitamos espacio final
+    l = f.read().replace('\n','').split(",")[0:-1]
+    #Convertimos a matriz de enteros y devolvemos
+    l = [[int(l[i*10+j]) for j in range(10) ]for i in range(10)]
+    return l
+
+
+m = cargaDatos("input/SOKOBOTTO1.txt")
+
+#Mostrado de datos
+for i in range(10):
+    for j in range(10):
+        print(m[i][j], end=',')
+    print()
+
+
+
+
